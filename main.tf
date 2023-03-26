0e8bb14643934791ffbad547297fdb7c46040580
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-26 12:14:52 +0530

    created launch template and ASG

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_launch_template" "main" {
 
     tags = merge(
       var.tags,
-      {Name = "${var.component}-${var.env}" }
+      { Name = "${var.component}-${var.env}" }
     )
   }
 
@@ -27,6 +27,8 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
+
+  name = "${var.component}-${var.env}"
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
@@ -35,5 +37,10 @@ resource "aws_autoscaling_group" "main" {
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    propagate_at_launch = false
+    value               = "${var.component}-${var.env}"
   }
 }
