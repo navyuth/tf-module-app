ee79d8461e1f16fe5a77c40a3ec2d8ba4e2756c7
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-26 12:05:12 +0530

    created launch template and ASG

--- vars.tf
+++ vars.tf
@@ -2,3 +2,7 @@ variable "env" {}
 variable "tags" {}
 variable "component" {}
 variable "instance_type" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "desired_capacity" {}
+variable "subnets" {}
