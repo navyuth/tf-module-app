9e7c9f1ce72b81a2ca470d6a1e3f39a0cda20739
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-04-01 22:23:28 +0530

    created target group

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_autoscaling_group" "main" {
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.main.id
@@ -81,4 +82,22 @@ resource "aws_security_group" "main" {
     var.tags,
     { Name = "${var.component}-${var.env}" }
   )
+}
+
+resource "aws_lb_target_group" "main" {
+  name = "${var.component}-${var.env}"
+  port = var.port
+  protocol = "HTTP"
+  vpc_id = var.vpc_id
+  health_check {
+    enabled = true
+    healthy_threshold = 2 # healthy threshold range 2-3
+    unhealthy_threshold = 5
+    interval = 5
+    timeout = 4
+  }
+    tags = merge(
+      var.tags,
+      { Name = "${var.component}-${var.env}" }
+    )
 }
